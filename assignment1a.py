ed8eca8ca41b9b0f39a164164dc160ba0c8ec803
WM1123-12 <william.meares@lhsla.org>
2026-08-31 09:01:51 -0700

    assignment 1a completed

---
+++ assignment1a.py
@@ -0,0 +1,17 @@
+# Meares, William
+# Computer Programming p4
+# Assignment 1a
+# August 27 2026
+
+print("Hello World!")
+print("Hello my name is William!")
+print("I love python!")
+print(16)
+print(5+5)
+print(4*3)
+print(50-40)
+print(100/5)
+print(100//5)
+# The extra slash means there will be no decimals in the answer
+print(2**4)
+print(6*3-4+8//2)
---
+++ assignment1b.py
@@ -0,0 +1,14 @@
+# Meares, William
+# Computer Programming, period 4
+# Assignment: Homework 2
+# 8/31/2026
+
+fav_hobby = "Drone Flying"
+print(fav_hobby)
+uncle = "David"
+print("hello", uncle)
+print(uncle.upper())
+print(uncle.lower())
+print(uncle.title())
+quote = "life is like riding a bycicle"
+print("Albert Einstien once said, ")
